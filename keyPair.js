f8caaaa2f6a354b5d77048984fbcf382a94d40c5
mrnazu <samuelamsalu1998@gmail.com>
2024-11-09 00:38:28 -0800

    private & public keys

--- keyPair.js
+++ keyPair.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 // private key
-
+// we're gonna randomly generate 256 pairs of random numbers
+// each 256 bits long
+// so our private key is 512 random numbers
 function genRandomNum(filename, numPairs) {
   const pairs = [];
   for (let i = 0; i < numPairs; i++) {
@@ -18,6 +20,7 @@ function genRandomNum(filename, numPairs) {
 genRandomNum('random_pairs.json', 256);
 
 // public key
+// for the public key, we're gonna hash out each numbers
 function genPublicKey(filename) {
   const privateKeyData = fs.readFileSync(filename, 'utf8');
   const privateKey = JSON.parse(privateKeyData);
